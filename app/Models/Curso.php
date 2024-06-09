5fb9a95f030aab817ed291255453b54741440e15
david <davidsanchezdiaz72@gmail.com>
2024-06-09 17:18:43 +0200

    Implementación de modelos Estudiante y Curso con relaciones muchos a muchos

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Definición de la clase Curso que extiende la clase Model de Eloquent
 class Curso extends Model
 {
+    // Uso del trait HasFactory para permitir la creación de instancias de modelo utilizando factories
     use HasFactory;
+
+    // Nombre de la tabla en la base de datos asociada al modelo Curso
+    public $table = "cursos";
+
+    // Lista de atributos que pueden ser asignados en masa (mass assignable)
+    protected $fillable = array("*");
+
+    // Método para definir la relación muchos a muchos entre el modelo Curso y el modelo Estudiante
+    public function estudiantes()
+    {
+        // Retorna la relación muchos a muchos definida con el modelo Estudiante y la tabla de pivote 'curso_estudiante'
+        return $this->belongsToMany(Estudiante::class, 'curso_estudiante');
+    }
 }
--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Definición de la clase Estudiante que extiende la clase Model de Eloquent
 class Estudiante extends Model
 {
+    // Uso del trait HasFactory para permitir la creación de instancias de modelo utilizando factories
     use HasFactory;
+
+    // Nombre de la tabla en la base de datos asociada al modelo Estudiante
+    public $table = "estudiantes";
+
+    // Lista de atributos que pueden ser asignados en masa (mass assignable)
+    protected $fillable = array("*");
+
+    // Método para definir la relación muchos a muchos entre el modelo Estudiante y el modelo Curso
+    public function cursos()
+    {
+        // Retorna la relación muchos a muchos definida con el modelo Curso y la tabla de pivote 'curso_estudiante'
+        return $this->belongsToMany(Curso::class, 'curso_estudiante');
+    }
 }
